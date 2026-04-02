dce7cfda3824e84b4ebea8997b6ee7b8a66cae8a
Sofiane <ls_yekene@esi.dz>
2026-04-02 15:33:21 +0200

    Fixed deployment for static files querying

--- pharmleder/urls.py
+++ pharmleder/urls.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lots.urls')),
+    path('static/<path:path>', serve, {'document_root': settings.STATICFILES_DIRS[0]}),
 ]
